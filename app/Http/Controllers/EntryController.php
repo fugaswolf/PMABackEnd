1ab01e70f5f1efb3715a76234d842ccd08957dca
fu6as <alvi.istamalov@gmail.com>
2019-03-31 22:40:18 +0200

    Add create method

--- app/Http/Controllers/EntryController.php
+++ app/Http/Controllers/EntryController.php
@@ -25,9 +25,27 @@ class EntryController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create($user_id, $start_time,$end_time,$duration,$description)
+    public function create(Request $request)
     {
-        $work = new Work($user_id,$start_time,$end_time,$duration,$description);
+        $request->validate([
+            'activity_id' => 'integer|required',
+            'user_id' => 'integer|required',
+            'description' => 'required|string',
+            'start_date' => 'required|string',
+            'end_date' => 'required|string',
+        ]);
+        $entry = new Entry([
+            'activity_id' => $request->activity_id,
+            'user_id' => $request->user_id,
+            'description' => $request->description,
+            'start_date' => $request->start_date,
+            'end_date' => $request->end_date,
+            'duration' => $entry->duration($request->start_date, $request->end_date)
+        ]);
+        $entry->save();
+        return response()->json([
+            'message' => 'Successfully created entry!'
+        ], 201);
     }
     /**
      * Store a newly created resource in storage.
@@ -47,26 +65,8 @@ class EntryController extends Controller
         ]);
         
         $work->save();
-        /* if($request->exists("tags")){
-            $tagstring = str_replace('"','',substr($request->input("tags"),1,-1));
-            $tagarray = explode(",",$tagstring);
-            foreach ($tagarray as $key => $val) {
-                $taglist = Tags::where("tag", $val)->first();
-                if ($taglist != null) {
-                    $tagid = $taglist->toArray()["id"];
-                    Work::orderBy('created_at', 'desc')->first()->tags()->attach($tagid);
-                } else {
-                    $thetag = (new Tags)->fill([
-                        'tag' => $val
-                    ]);
-                    $thetag->save();
-                    Work::orderBy('created_at', 'desc')->first()->tags()->save($thetag);
-                }
-            }
-        } */
         return "succes";
-        //return print_r($thetag);
-        //return new WorkResource($work);
+
     }
     public function firstOrResponse($field, $value) {
         return ($entry = Entry::where($field, 'LIKE', $value)->get()) instanceof Entry ?
@@ -74,12 +74,14 @@ class EntryController extends Controller
                 "Entry not found with $field: $value"
             ], 404);
     }
+
     public function searchLIKE($field, $value) {
         return ($entry = Entry::where($field, 'LIKE', $value)->get()) instanceof Entry ?
             new EntryResource($entry) : response()->json([
                 "Entry not found with $field: $value"
             ], 404);
     }
+
     /**
      * Display the specified resource.
      *
@@ -89,20 +91,19 @@ class EntryController extends Controller
     public function show($id)
     {
         $entry = Entry::where('id', '=', $id)->firstOrFail();
-        // $entry = Entry::with('tags')->where('id', '=', $id)->firstOrFail();
-        //dd($work);
         return new EntryResource($entry);
-        //return $this->firstOrResponse('finalworkID', $id);
     }
+
     public function showByProject($project)
     {
         return Entry::where('project_id', 'LIKE', '%'.$project.'%')->get();
-        /*return $this->firstOrResponse('finalworkTitle',  $title);*/
+
     }
+
     public function showByDivision($division)
     {
         return Entry::where('division', 'LIKE', '%'.$division.'%')->get();
-        /*return $this->firstOrResponse('departement', $departement);*/
+
     }
     /**
      * Show the form for editing the specified resource.
