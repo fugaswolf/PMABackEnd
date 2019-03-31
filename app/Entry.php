f820283749dd7e0766e665430432dab4ed731701
fu6as <alvi.istamalov@gmail.com>
2019-03-31 22:39:21 +0200

    Time Diff Calculator methode added

--- app/Entry.php
+++ app/Entry.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App;
-
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Entry extends Model
@@ -10,8 +10,14 @@ class Entry extends Model
 
     // @TODO
     protected $fillable = [
-        'activity_id'
+        'activity_id', 'user_id', 'start_date', 'end_date', 'duration', 'description'
     ];
+
+    public function duration($start, $end){
+        $duration = $end->diff($start)->format('%H:%I:%s');
+
+        $this->attributes['duration'] = $duration;
+    }
 
     public function user(){
         return $this->belongsTo(\App\User::class);
