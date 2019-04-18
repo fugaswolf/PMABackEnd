931569bd6384696a4c12c487d726e4c1e831562a
fu6as <alvi.istamalov@gmail.com>
2019-04-18 16:45:05 +0200

    User roles update fixed

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -123,9 +123,6 @@ class UserController extends Controller
 
         $role = $request->roles;
 
-        // doe nog eens die request om te zien of die role er is
-        // hoezo?
-        //dd($role);
 
         if ( ! $user->hasRole($role)) {
             // verwijder alle voorgaande rollen
@@ -151,52 +148,59 @@ class UserController extends Controller
             'message' => User::where('id', '=', $id)->firstOrFail()->delete() ? 'Success.' : 'Failed.'
         ]);
     }
-    public function search(Request $request)
-    {
-        $works = (new Work)->newQuery()->with('tags');
-        $filterArray = $this->checkFilters($request);
-        $keywords = $this->divideSearchQuery($request);
-        $results = $works->where(function($query) use ($request, $filterArray, $keywords) {
-            $query->where($filterArray);
-            $query->where(function ($query) use ($keywords) {
-                for ($i = 0; $i < count($keywords); $i++) {
-                    $query->where('finalworkTitle', 'LIKE', '%' . $keywords[$i] . '%');
-                    $query->orWhere('finalworkDescription', 'LIKE', '%' . $keywords[$i] . '%');
-                    $query->whereTagsLike($keywords[$i]);
-                }
-            });
-        })->get();
-        return $results;
-    }
-    private function divideSearchQuery($request) {
-        if ($request->has('q')) {
-            $keyword = $request->input('q');
-            $keywords = explode(" ", $keyword);
-        } else {
-            $keywords = [];
-        }
-        return $keywords;
-    }
-    private function checkFilters($request) {
-        $filterArray = array();
-        if ($request->has('department')) {
-            array_push($filterArray, ['departement', '=', $request->Input('department')]);
-        }
-        if ($request->has('author')) {
-            array_push($filterArray, ['finalworkAuthor', 'LIKE', '%' . $request->input('author') . '%']);
-        }
-        if ($request->has('field')) {
-            array_push($filterArray, ['finalworkField', 'LIKE',  '%' . $request->input('field') . '%']);
-        }
-        if ($request->has('promotor')) {
-            array_push($filterArray, ['finalworkPromoter', 'LIKE',  '%' . $request->input('promotor') . '%']);
-        }
-        if ($request->has('maxYear') || $request->has('minYear')) {
-            $maxYear = $request->has('maxYear') == null ? ['finalworkYear', '<=', date('Y')] : ['finalworkYear', '<=', $request->input('maxYear')];
-            $minYear = $request->has('minYear') == null ? ['finalworkYear', '>=', 1980] : ['finalworkYear', '>=', $request->input('minYear')];
-            array_push($filterArray, $minYear);
-            array_push($filterArray, $maxYear);
-        }
-        return $filterArray;
-    }
+
+
+    
+    // public function search(Request $request)
+    // {
+    //     $works = (new Work)->newQuery()->with('tags');
+    //     $filterArray = $this->checkFilters($request);
+    //     $keywords = $this->divideSearchQuery($request);
+    //     $results = $works->where(function($query) use ($request, $filterArray, $keywords) {
+    //         $query->where($filterArray);
+    //         $query->where(function ($query) use ($keywords) {
+    //             for ($i = 0; $i < count($keywords); $i++) {
+    //                 $query->where('finalworkTitle', 'LIKE', '%' . $keywords[$i] . '%');
+    //                 $query->orWhere('finalworkDescription', 'LIKE', '%' . $keywords[$i] . '%');
+    //                 $query->whereTagsLike($keywords[$i]);
+    //             }
+    //         });
+    //     })->get();
+    //     return $results;
+    // }
+
+    // private function divideSearchQuery($request) {
+    //     if ($request->has('q')) {
+    //         $keyword = $request->input('q');
+    //         $keywords = explode(" ", $keyword);
+    //     } else {
+    //         $keywords = [];
+    //     }
+    //     return $keywords;
+    // }
+
+
+
+    // private function checkFilters($request) {
+    //     $filterArray = array();
+    //     if ($request->has('department')) {
+    //         array_push($filterArray, ['departement', '=', $request->Input('department')]);
+    //     }
+    //     if ($request->has('author')) {
+    //         array_push($filterArray, ['finalworkAuthor', 'LIKE', '%' . $request->input('author') . '%']);
+    //     }
+    //     if ($request->has('field')) {
+    //         array_push($filterArray, ['finalworkField', 'LIKE',  '%' . $request->input('field') . '%']);
+    //     }
+    //     if ($request->has('promotor')) {
+    //         array_push($filterArray, ['finalworkPromoter', 'LIKE',  '%' . $request->input('promotor') . '%']);
+    //     }
+    //     if ($request->has('maxYear') || $request->has('minYear')) {
+    //         $maxYear = $request->has('maxYear') == null ? ['finalworkYear', '<=', date('Y')] : ['finalworkYear', '<=', $request->input('maxYear')];
+    //         $minYear = $request->has('minYear') == null ? ['finalworkYear', '>=', 1980] : ['finalworkYear', '>=', $request->input('minYear')];
+    //         array_push($filterArray, $minYear);
+    //         array_push($filterArray, $maxYear);
+    //     }
+    //     return $filterArray;
+    // }
 }
