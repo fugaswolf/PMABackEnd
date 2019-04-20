5a397f5b74cc8d6ae2b0f11f4861ceb44b076031
fu6as <alvi.istamalov@gmail.com>
2019-04-20 20:54:11 +0200

    updated Entry factory

--- database/factories/EntryFactory.php
+++ database/factories/EntryFactory.php
@@ -4,11 +4,10 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Entry::class, function (Faker $faker) {
     return [
-        'id' => $faker->numberBetween(),
         'user_id' => $faker->randomNumber(),
         'start_date' => $faker->date(),
         'end_date' => $faker->date(),
-        'duration' => $faker->randomNumber(2), // het verschil tussen start en end date? hoe doe ik dit? maakt niet uit tis toch maar dummy data
+        'duration' => $faker->randomNumber(2),
         'description' => $faker->realText(),
         'activity_id' => $faker->randomNumber()
     ];
