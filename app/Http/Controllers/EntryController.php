f1e5ba3596ec7c32a4f36e99ef80c656d97a7c49
fu6as <alvi.istamalov@gmail.com>
2019-04-20 20:49:30 +0200

    sync data from Mobile, show method with roles, fixed edit entries

--- app/Http/Controllers/EntryController.php
+++ app/Http/Controllers/EntryController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Entry;
-use App\Http\Resources\EntryResource;
 use Auth;
+use App\Entry;
+use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
+use App\Http\Resources\EntryResource;
 
 class EntryController extends Controller
 {
@@ -15,7 +16,7 @@ class EntryController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function index()
-    {
+    {   
         //Get entries
         $entries = Entry::paginate(10);
     
@@ -35,10 +36,10 @@ class EntryController extends Controller
             'description' => 'required|string',
             'start_date' => 'required|string',
             'end_date' => 'required|string',
-            'duration' => 'nullable|string'
+            //'duration' => 'nullable|string'
         ]);
 
-        $duration = $request->has('duration') && !is_null($request->input('duration')) ? : Entry::parseDuration($request->input('start_date'), $request->input('end_date'));
+        //$duration = $request->has('duration') && !is_null($request->input('duration')) ? : Entry::parseDuration($request->input('start_date'), $request->input('end_date'));
         
         (new Entry)->create([
             'activity_id' => $request->activity_id,
@@ -46,7 +47,7 @@ class EntryController extends Controller
             'description' => $request->description,
             'start_date' => $request->start_date,
             'end_date' => $request->end_date,
-            'duration' => $duration
+            //'duration' => $duration
         ]);
         
         return response()->json([
@@ -77,12 +78,6 @@ class EntryController extends Controller
     public function show()
     {
         $user = Auth::user();
-
-        // projectnaam, customernaam, activitynaam
-        // waar zit de project fzo ? 
-        // project zou je kunnen ophalen via de activity (er is een project_id in de tabel activities, en customer haal je op via project fso (customer_id))
-
-        // ok dit moet werken
         $entry = Entry::with('activity:id,name,project_id', 'activity.project:id,name,customer_id', 'activity.project.customer:id,name')->where('user_id', '=', $user->id)->latest()->get();
 
 
@@ -92,9 +87,23 @@ class EntryController extends Controller
     public function showPG()
     {
         $user = Auth::user();
-        $entry = Entry::where('user_id', '=', $user->id)->latest()->paginate(15);
+        $entry = Entry::with('activity', 'activity.project')->where('user_id', '=', $user->id)->latest()->paginate(15);
         dd($entry);
         return new EntryResource($entry);
+    }
+
+    public function showAll(){
+        $user = Auth::user();
+
+        $entries = null;
+        
+        if ($user->hasAnyRole('admin|manager|hr')) {
+            $entries = Entry::with('activity', 'activity.project', 'user')->latest()->paginate(15);
+        } else {
+            $entries = Entry::with('activity', 'activity.project')->where('user_id', '=', $user->id)->latest()->paginate(15);
+        }
+
+        return EntryResource::collection($entries);
     }
 
     public function showByProject($project)
@@ -133,16 +142,14 @@ class EntryController extends Controller
 
         $entry->fill([
             'activity_id'=>   $request->input('activity_id'),
-            'user_id'=>   $user->id, // toch ? jup
+            'user_id'=>   $user->id,
             'description'=>  $request->input('description'),
             'start_date'=>  $request->input('start_date'),
             'end_date' => $request->input('end_date'),
         ]);
 
-        if ($request->has('duration') && !is_null($duration = $request->input('duration'))) {
-            $entry->fill(compact('duration'));
-        }
-
+        // damn
+    
         $entry->save();
         return new EntryResource($entry);
       
@@ -157,6 +164,33 @@ class EntryController extends Controller
     {
         return response()->json([
             'message' => Entry::where('id', '=', $id)->firstOrFail()->delete() ? 'Success.' : 'Failed.'
+        ]);
+    }
+
+    /**
+     * Sync entries from json data
+     */
+    public function syncAll(Request $request) {
+        $data = $request->json()->get('data');
+
+        foreach (/*$actualData*/ $data as $entry){
+            \App\Entry::updateOrCreate(
+                [
+                    'id' => $entry['id'] ?? null // ez fix
+                ],
+                [
+                    'user_id' => $entry['user_id'],
+                    'start_date' => $entry['start_date'],
+                    'end_date' => $entry['end_date'],
+                    'duration' => $entry['duration'],
+                    'description' => $entry['description'],
+                    'activity_id' => $entry['activity_id']
+                ]
+            );
+        }
+        
+        return response()->json([
+            'message' => 'Data succesfully synced'
         ]);
     }
 }
